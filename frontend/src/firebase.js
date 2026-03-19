6050294e731aa63a04441e6bf6afab71aa45aecb
Kratika Gupta <therealkratikagupta@gmail.com>
2026-03-19 23:16:40 +0530

    Update Firebase config with placeholder values
    Replaced sensitive Firebase configuration values with placeholders.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import{getAuth} from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIWJPyNU680Q7zIpoRFFjjOa16E8BkbdQ",
+  apiKey: "APIKEY",
   authDomain: "driveox-24950.firebaseapp.com",
   projectId: "driveox-24950",
   storageBucket: "driveox-24950.firebasestorage.app",
   messagingSenderId: "908348644985",
-  appId: "1:908348644985:web:431d38116118edd6ce3ea8",
+  appId: "APP_ID",
   measurementId: "G-J9SJNNL6L3"
 };
 
